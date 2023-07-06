9005a96d1993b61c41736f4bc6ccc2968c6e3b78
thomaseade <eadethomas95@gmail.com>
2023-07-06 15:22:48 +0200

    arreglos

--- Dia1/CapturasReto1/index.js
+++
@@ -1,7 +0,0 @@
-let mongoose = require ("mongoose");
-let User = require ("./userMDB")
-
-mongoose.connect('mongodb+srv://eadethomas:esklavos100@retomongo.0p0fsfh.mongodb.net/Codenotch', 
-                  {useNewUrlParser: false, useUnifiedTopology: false});
-                  
-
